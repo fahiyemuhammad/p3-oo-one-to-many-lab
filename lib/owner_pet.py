b648e11f94fecccc98b29d024056c698fcec3756
Fahiye Muhammad <fahiye254@gmail.com>
2025-05-15 19:51:21 +0300

    Learning Object relationships in python

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,60 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+    def pets(self):
+        return Pet.all     
+    
+    def add_pet(self,pet):
+        if isinstance(pet,Pet):
+            pet.owner = self
+        else :
+            raise Exception("Invalid pet. Must be a Pet instance.")
+    
+
+    def get_sorted_pets(self):
+        return sorted(
+            [pet for pet in Pet.all if pet.owner == self],
+            key = lambda pet : pet.name
+        )
+
+
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self,name,pet_type,owner = ""):
+        self.name = name 
+        self.owner = owner
+
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+            Pet.all.append(self)
+        else :
+            raise Exception(f"Invalid pet type: {pet_type}. Must be one of {Pet.PET_TYPES}")
+    
+
+    
+
+
+
+
+
+
+
+
+          
+
+
+
+
+    
+
+
+
+
+
+
+        
